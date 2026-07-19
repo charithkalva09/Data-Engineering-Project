7a577da92165b4bdfe7add9d75d75080ad1def9b
charithkalva09 <charithreddykalva@gmail.com>
2026-07-19 19:00:24 -0400

    Update quality_checks_gold.sql

--- Quality Tests/quality_checks_gold.sql
+++ Quality Tests/quality_checks_gold.sql
@@ -1,5 +1,4 @@
 /*
-===============================================================================
 Quality Checks
 ===============================================================================
 Script Purpose:
@@ -14,11 +13,10 @@ Usage Notes:
 ===============================================================================
 */
 
--- ====================================================================
+
 -- Checking 'gold.dim_customers'
 -- ====================================================================
 -- Check for Uniqueness of Customer Key in gold.dim_customers
--- Expectation: No results 
 SELECT 
     customer_key,
     COUNT(*) AS duplicate_count
@@ -26,11 +24,10 @@ FROM gold.dim_customers
 GROUP BY customer_key
 HAVING COUNT(*) > 1;
 
--- ====================================================================
+
 -- Checking 'gold.product_key'
 -- ====================================================================
 -- Check for Uniqueness of Product Key in gold.dim_products
--- Expectation: No results 
 SELECT 
     product_key,
     COUNT(*) AS duplicate_count
@@ -38,7 +35,7 @@ FROM gold.dim_products
 GROUP BY product_key
 HAVING COUNT(*) > 1;
 
--- ====================================================================
+
 -- Checking 'gold.fact_sales'
 -- ====================================================================
 -- Check the data model connectivity between fact and dimensions
@@ -48,4 +45,4 @@ LEFT JOIN gold.dim_customers c
 ON c.customer_key = f.customer_key
 LEFT JOIN gold.dim_products p
 ON p.product_key = f.product_key
-WHERE p.product_key IS NULL OR c.customer_key IS NULL  
+WHERE p.product_key IS NULL OR c.customer_key IS NULL
